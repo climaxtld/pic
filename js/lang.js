707208a29c52c52b2fc726bb242098caef4e3527
climaxtld <19236664+climaxtld@users.noreply.github.com>
2023-11-11 12:41:45 +0800

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,18 @@
+const lang = {
+    cn: {
+        'title': '什么是PicWe',
+        'intro-1': '让交易更简单',
+        'intro-2': '在web3中创建新的社交模式',
+        'intro-3': 'Web3交易图表社交',
+        'trading': '开始交易',
+        "home":"首页"
+    },
+    en: {
+        'title': 'What is PicWe',
+        'intro-1': 'Make Trading Easier',
+        'intro-2': 'Create the New Social Model in Web3',
+        'intro-3': 'Web3 Chart Social',
+        'trading': 'Start Trading',
+        "home":"Home"
+    },
+}
